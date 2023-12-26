2471376f32c33e68d3c02e238faee3e314a580ab
Palvovets Alexander <alex.pavloveccccc@gmail.com>
2023-12-26 16:38:53 +0300

    [TT-1] Deploy new Xcode project

---
+++ CarTest/CarTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarTest
+//
+//  Created by Alexander Pavlovets on 26.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
