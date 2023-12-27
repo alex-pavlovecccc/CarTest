545236e403d164b2445f4f2d95df62a163645deb
Palvovets Alexander <alex.pavloveccccc@gmail.com>
2023-12-27 17:22:40 +0300

    Create coordinator and update sceneDelegate

---
+++ CarTest/CarTest/Coordinator/AppCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  AppCoordinator.swift
+//  CarTest
+//
+//  Created by Alexander Pavlovets on 27.12.2023.
+//
+
+import Foundation
---
+++ CarTest/CarTest/Coordinator/Coordinator.swift
@@ -0,0 +1,8 @@
+//
+//  Coordinator.swift
+//  CarTest
+//
+//  Created by Alexander Pavlovets on 27.12.2023.
+//
+
+import Foundation
